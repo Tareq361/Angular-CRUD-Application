6a0ad8a0c8e9c1900fbbe9df27d1ee0620a66daa
Tareq361 <mtareq361@gmail.com>
2022-08-28 15:58:06 +0600

    All Books component creted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddBookComponent } from './Book/add-book/add-book.component';
+import { UpdateBookComponent } from './Book/update-book/update-book.component';
+import { DeleteBookComponent } from './Book/delete-book/delete-book.component';
+import { ListBooksComponent } from './Book/list-books/list-books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBookComponent,
+    UpdateBookComponent,
+    DeleteBookComponent,
+    ListBooksComponent
   ],
   imports: [
     BrowserModule
